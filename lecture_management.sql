af9e3f1afac6ddae120b924f0c0190dd298b5319
lsawfa <laryntsawfakenanga@gmail.com>
2024-01-03 07:55:40 +0700

    [ADD] Database lecture_management.sql

--- lecture_management.sql
+++ lecture_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 31, 2023 at 12:10 PM
+-- Generation Time: Jan 01, 2024 at 01:32 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `lecture_management`
 --
+CREATE DATABASE IF NOT EXISTS `lecture_management` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `lecture_management`;
 
 -- --------------------------------------------------------
 
@@ -33,22 +35,22 @@ CREATE TABLE `aktivitas` (
   `tahun_ajaran` varchar(255) NOT NULL,
   `tanggal_mulai` date NOT NULL,
   `tanggal_berakhir` date NOT NULL,
-  `jumlah_volume_kegiatan` double NOT NULL,
-  `sks_hasil` int(11) NOT NULL,
   `path_lampiran` varchar(255) NOT NULL,
   `no_penugasan` varchar(255) DEFAULT NULL,
   `jenis_lampiran` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `angka_kredit` double NOT NULL
+  `angka_kredit` double NOT NULL,
+  `jumlah_volume_kegiatan` double DEFAULT NULL,
+  `sks_hasil` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `aktivitas`
 --
 
-INSERT INTO `aktivitas` (`id`, `nama_aktivitas`, `tahun_ajaran`, `tanggal_mulai`, `tanggal_berakhir`, `jumlah_volume_kegiatan`, `sks_hasil`, `path_lampiran`, `no_penugasan`, `jenis_lampiran`, `user_id`, `angka_kredit`) VALUES
-(15, 'Perkuliahan', 'Ganjil 2023/2024', '2023-03-20', '2023-03-20', 10, 50, 'zpNisLXitHDR8y9ZVx42SpK0JSyt6ZuhlaE1tXJK.pdf', '1902001', 'SK', 2, 0.5),
-(16, 'Perkuliahan', 'Genap 2023/2024', '2024-01-01', '2024-03-06', 3, 24, 'Dx5NXDzGrX5ItAMEpAwOqxNIQB3qqkMOLXFjyAFc.pdf', '1902012', 'SK', 2, 0.8);
+INSERT INTO `aktivitas` (`id`, `nama_aktivitas`, `tahun_ajaran`, `tanggal_mulai`, `tanggal_berakhir`, `path_lampiran`, `no_penugasan`, `jenis_lampiran`, `user_id`, `angka_kredit`, `jumlah_volume_kegiatan`, `sks_hasil`) VALUES
+(19, 'Perkuliahan', 'Ganjil 2023/2024', '2024-01-01', '2024-01-08', 'H8ZvbmToOkwM49eEV6yJF1pA1GDR3TeZbLvbpaWu.pdf', 'PK-002', 'SK', 2, 4, 4, 4),
+(20, 'Perkuliahan', 'Genap 2023/2024', '2024-01-01', '2024-01-29', 'kMXLu7FGrn7HxvGiXqZldXEPzxI8rVKc73ZtsduN.pdf', 'PK-001', 'SK', 2, 2, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -82,13 +84,6 @@ CREATE TABLE `jabatans` (
   `user_id` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `jabatans`
---
-
-INSERT INTO `jabatans` (`id`, `jabatan`, `periode_awal`, `periode_akhir`, `no_sk`, `lampiran_jabatan`, `user_id`) VALUES
-(2, 'Rektor', '2023-12-01', '2023-12-31', 'RK-001', 'WbxJc7tennyehCHNR6M3bJRSclBXKlOsJA8ZY6WA.pdf', '2');
-
 -- --------------------------------------------------------
 
 --
@@ -110,9 +105,8 @@ CREATE TABLE `kelas_aktivitas` (
 --
 
 INSERT INTO `kelas_aktivitas` (`id`, `kelas_matkul`, `kelas_nama`, `kelas_waktu`, `kelas_sks`, `aktivitas_id`, `kelas_hari`) VALUES
-(7, 'Pemrograman Web', 'Pemrograman Web E', '13:32:00', 3, 15, 2),
-(8, 'Pemrograman Fungsional', 'Pemrograman Fungsional F', '13:34:00', 3, 15, 3),
-(9, 'Sistem Operasi', 'Sistem Operasi A', '13:36:00', 2, 16, 4);
+(10, 'Pratikum', 'Web E', '00:09:00', 2, 19, 2),
+(11, 'Sistem Operasi', 'A', '08:15:00', 2, 20, 2);
 
 -- --------------------------------------------------------
 
@@ -162,13 +156,6 @@ CREATE TABLE `orasi_ilmiahs` (
   `user_id` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `orasi_ilmiahs`
---
-
-INSERT INTO `orasi_ilmiahs` (`id`, `tema_orasi`, `waktu`, `lampiran_orasi`, `user_id`) VALUES
-(3, 'Optimasi Keberhasilan Akuakultur melalui Implementasi Convolutional Neural Networks (CNNs) dalam Deteksi White Spot Syndrome Virus pada Udang Vaname: Inovasi Image Processing Deep Learning untuk Peningkatan Kesehatan dan Produktivitas', '2023-12-31', 'mRikWyaddePLoX4iOW3Tcf3aBL70lLSUCg1kIedf.pdf', '2');
-
 -- --------------------------------------------------------
 
 --
@@ -198,13 +185,6 @@ CREATE TABLE `pembimbings` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `pembimbings`
---
-
-INSERT INTO `pembimbings` (`id`, `nama_kegiatan`, `tahun_ajaran`, `waktu`, `no_sk`, `path_lampiran`, `path_foto_kegiatan`, `user_id`) VALUES
-(3, 'Gemastik', 'Ganjil 2023/2024', '2023-12-31 00:00:00', 'GM-02', 'YlYVyL7CQtkOg64NP0MM78LVZzd7ZFocrvpq1Afc.pdf', 'syUYkyew5vu2peHM5uFuPKOgCgnZloh0iKk7VJtR.pdf', 2);
-
 -- --------------------------------------------------------
 
 --
@@ -229,7 +209,7 @@ CREATE TABLE `pendampings` (
 --
 
 INSERT INTO `pendampings` (`id`, `mahasiswa_nim`, `mahasiswa_nama`, `jenis_kegiatan`, `tanggal_dimulai`, `tanggal_berakhir`, `no_sk`, `path_lampiran`, `path_foto_kegiatan`, `user_id`) VALUES
-(4, '202110370311187', 'Salsabila', 'Magang', '2023-12-31 00:00:00', '2024-01-04 00:00:00', 'KKN-01', 'xYQcooXgn50Bv20Kqm5gKnoshMqapJWdx47VgtEk.pdf', 'nie75pSsxuIghiMEuR8P30sA2b9pNaXevdorYc7P.pdf', 2);
+(5, '202110370311187', 'Aya', 'KKN', '2024-01-01 00:00:00', '2024-01-29 00:00:00', 'KKN-001', 's8Vztt7ntYl1MCGET04JHOk2qLkntfMlrbzEOadD.pdf', 'hhmweVF9FD0VfGx3kSLYr8Hmx3vwj7IOlZuUltnW.pdf', 2);
 
 -- --------------------------------------------------------
 
@@ -245,13 +225,6 @@ CREATE TABLE `pengembangan_bahan_ajars` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `pengembangan_bahan_ajars`
---
-
-INSERT INTO `pengembangan_bahan_ajars` (`id`, `jenis_bahan_ajar`, `deskripsi_bahan_ajar`, `hasil_pengembangan`, `user_id`) VALUES
-(3, 'Web', 'Using Laravel', 'VViQ0lTTM2sBAvK5W7BaaVO7FduUyZimOOtiMH7q.pdf', 2);
-
 -- --------------------------------------------------------
 
 --
@@ -265,13 +238,6 @@ CREATE TABLE `pengembangan_p_k_s` (
   `hasil_pengembangan` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `pengembangan_p_k_s`
---
-
-INSERT INTO `pengembangan_p_k_s` (`id`, `matkul_pengembangan`, `deskripsi_pengembangan`, `hasil_pengembangan`, `user_id`) VALUES
-(3, 'Etika dan Profesi', 'Transformasi Praktikum Fakultas Kedokteran Hewan: Merintis Masa Depan Pendidikan dengan Teknologi Virtual Reality di FKH UB untuk Peningkatan Pemahaman dan Keterampilan Mahasiswa', 'NPgddPVmHAUjuRqCN9dRQ632hpKlsmYl0Myi1BMR.pdf', 2);
 
 -- --------------------------------------------------------
 
@@ -292,13 +258,6 @@ CREATE TABLE `pengujis` (
   `path_foto_kegiatan` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `pengujis`
---
-
-INSERT INTO `pengujis` (`id`, `mahasiswa_nim`, `mahasiswa_nama`, `jenis_ujian_akhir`, `posisi_penguji`, `tahun_ajaran`, `waktu`, `no_sk`, `path_lampiran`, `path_foto_kegiatan`, `user_id`) VALUES
-(3, '202110370311187', 'Ahya', 'Skripsi', 'Penguji 2', 'Ganjil 2023/2024', '2023-12-31 00:00:00', 'TA-004', 'FNRqFptCy3mUmMzHrFRHQv7sk1BiiSmsiKJnJP6g.pdf', 'OWtwcFx1txUOxb1Yn3NoIosA1bmSAu01Q9TTlbWc.pdf', 2);
 
 -- --------------------------------------------------------
 
@@ -337,13 +296,6 @@ CREATE TABLE `seminars` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `seminars`
---
-
-INSERT INTO `seminars` (`id`, `mahasiswa_nim`, `mahasiswa_nama`, `tahun_ajaran`, `waktu_seminar`, `no_berita_acara`, `path_lampiran`, `path_foto_kegiatan`, `user_id`) VALUES
-(8, '202110370311189', 'Sawfa', 'Ganjil 2023/2024', '2023-12-31 00:00:00', 'TA-002', 'N1Qr4qSPNTw0QmyhLxxiiUlFi5wZSVPOeW7XuWLa.pdf', 'b7yCZiyxCaoIxDONPJykceulkgJxwqMe1QkM2HkR.pdf', 2);
-
 -- --------------------------------------------------------
 
 --
@@ -364,13 +316,6 @@ CREATE TABLE `tugas_akhirs` (
   `path_foto_kegiatan` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `tugas_akhirs`
---
-
-INSERT INTO `tugas_akhirs` (`id`, `mahasiswa_nim`, `mahasiswa_nama`, `jenis_bimbingan`, `tahun_ajaran`, `tanggal_dimulai`, `tanggal_berakhir`, `jenis_pembimbing`, `no_sk`, `path_lampiran`, `path_foto_kegiatan`, `user_id`) VALUES
-(3, '2021103703111189', 'Kenanga', 'Thesis', 'Ganjil 2023/2024', '2023-12-24 00:00:00', '2023-12-31 00:00:00', 'Pembimbing 1', 'TA-002', 'AO9RB5r54wJDb3sSiNcXy33QfWmGhl43nUZMhVPV.pdf', 'UVwxIL7rwzqqjad21IUqp7qxIAFN7knYMCpyf9Bw.pdf', 2);
 
 -- --------------------------------------------------------
 
@@ -394,8 +339,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Sarla', 'sarla@gmail.com', NULL, '$2a$12$Ldtp8z9zBYEZHbGuGjuSG.m3dWoC6/mAfpU3QF9tKpbl12JW5PdgS', NULL, NULL, NULL),
 (2, 'larynt', 'larynt@gmail.com', NULL, '$2a$12$GWkO3SQI9pem1scWU1.kVOoS2OzUrpngMKp0cITrBUgBop7.EQ1WS', NULL, NULL, NULL),
-(3, 'ahya', 'ahya@gmail.com', NULL, '$2a$12$GWkO3SQI9pem1scWU1.kVOoS2OzUrpngMKp0cITrBUgBop7.EQ1WS', NULL, NULL, NULL);
+(3, 'ahya', 'ahya@gmail.com', NULL, '$2a$12$.ejqLyHTdT2f9fvKC50cQubdjf3LCtYUfOaAiW3.X6NheumbTYGZy', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -423,6 +369,7 @@ CREATE TABLE `user_data` (
 --
 
 INSERT INTO `user_data` (`id`, `user_name`, `user_tempat_lahir`, `user_tanggal_lahir`, `user_nidn`, `user_golongan`, `user_pangkat`, `user_pendidikan_terkahir`, `user_pendidikans1`, `user_pendidikans2`, `user_pendidikans3`, `user_id`) VALUES
+(1, 'Sarla', 'Lombok', '2023-12-01', '1902831', '3', 'Dosen', 'Universitas Muhammadiyah Malang', 'Universitas Muhammadiyah Malang', 'Universitas Muhammadiyah Malang', 'Universitas Muhammadiyah Malang', 1),
 (2, 'Larynt', 'Lamongan', '2013-12-04', '19287109281', '1', 'Rektor', 'S3', 'Universitas Muhammadiyah Malang', 'Institut Teknologi Sepuluh Nopember', 'Institut Teknologi Bandung', 2),
 (3, 'ahya', 'Tarakan', '2013-12-04', '19287109281', '2', 'Dekan', 'S3', 'Universitas Muhammadiyah Malang', 'Institut Teknologi Sepuluh Nopember', 'Institut Teknologi Bandung', 3);
 
@@ -544,7 +491,7 @@ ALTER TABLE `user_data`
 -- AUTO_INCREMENT for table `aktivitas`
 --
 ALTER TABLE `aktivitas`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -562,7 +509,7 @@ ALTER TABLE `jabatans`
 -- AUTO_INCREMENT for table `kelas_aktivitas`
 --
 ALTER TABLE `kelas_aktivitas`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -580,13 +527,13 @@ ALTER TABLE `orasi_ilmiahs`
 -- AUTO_INCREMENT for table `pembimbings`
 --
 ALTER TABLE `pembimbings`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `pendampings`
 --
 ALTER TABLE `pendampings`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `pengembangan_bahan_ajars`
@@ -616,7 +563,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `seminars`
 --
 ALTER TABLE `seminars`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tugas_akhirs`
